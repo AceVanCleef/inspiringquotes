3d7769a1b7e82b6f15d50c2a59ced9cf76de47e6
Stefan Wohlgensinger <stefan.wohlgensinger@gmail.com>
2026-05-04 23:41:25 +0200

    vercel routing fix: exposing bff layer url

--- frontend/config/env.ts
+++ frontend/config/env.ts
@@ -2,7 +2,7 @@ export const ENV = {
   /** defines the route to the backend API */
   API_URL: process.env.API_URL,
   /** defines the route to the backend-for-frontend layer */
-  BFF_LAYER_URL: process.env.BFF_LAYER_URL,
+  BFF_LAYER_URL: process.env.NEXT_PUBLIC_BFF_LAYER_URL,
   /** defines the access key to the backend API */
   API_KEY: process.env.CLIENT_API_KEY,
   // Hier können später weitere Variablen hin, z.B. ANALYTICS_ID etc.
