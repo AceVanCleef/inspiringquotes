694e06c3ae1822e522c5a7aaf732032bcac8a031
Stefan Wohlgensinger <stefan.wohlgensinger@gmail.com>
2026-05-04 23:34:54 +0200

    vercel server pre-render fix 2

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,6 +2,8 @@ import QuoteCard from '@/components/ui/atom/QuoteCard';
 import QuotesList from '@/components/ui/molecule/QuotesList'
 import { getDailyQuote, getPopularQuotes, getRecentQuotes } from '@/lib/api';
 
+export const dynamic = 'force-dynamic';
+
 export default async function Home() {
   const [dailyQuote, popularQuotes, recentQuotes] = await Promise.all([
           getDailyQuote(),
